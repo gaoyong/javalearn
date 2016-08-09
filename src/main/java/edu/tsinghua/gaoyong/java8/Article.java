f22236ae96b3b17717ee7733c59b85faac9ec864
gaoyong <519582737@qq.com>
2016-08-09 14:51:47 +0800

    add some demo for java8

---
+++ src/main/java/edu/tsinghua/gaoyong/java8/Article.java
@@ -0,0 +1,49 @@
+package edu.tsinghua.gaoyong.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Article {
+	
+	private  String title;
+    private  String author;
+    private  List<String> tags;
+    
+    public Article(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Article(String title, String author, List<String> tags) {
+        this.title = title;
+        this.author = author;
+        this.tags = tags;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public static void main(String[] args){
+    	List<Article> articleList = new ArrayList<>();
+    	for(int i =0 ;i < 5;i++){
+    		Article article = new Article("title"+i,"author"+i);
+    		articleList.add(article);
+    	}
+    	
+//    	articleList.stream().findFirst();
+    	
+    	articleList.stream()
+        .filter(article -> article.getTitle().contains("Java"))
+        .findFirst();
+    }
+    
+}
